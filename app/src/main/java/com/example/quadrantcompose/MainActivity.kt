2375777dfce7d2c92c8c0f9486739598f3f17b3c
newton225 <isaacndri@gmail.com>
2026-08-23 13:57:21 +0000

    fix: deplacement des textes dans Ressource

--- app/src/main/java/com/example/quadrantcompose/MainActivity.kt
+++ app/src/main/java/com/example/quadrantcompose/MainActivity.kt
@@ -7,13 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -84,16 +82,16 @@ fun QuadrantCompose(modifier: Modifier = Modifier) {
             modifier = Modifier.weight(1f)
         ) {
             Quadrant(
-                title = "Text composable",
-                description = "Displays text and follows the recommended Material Design guidelines",
+                title = stringResource(R.string.titre1),
+                description = stringResource(R.string.description1),
                 backgroundColor = Color(0xFFEADDFF),
                 modifier = Modifier
                     .weight(1f)
                     .fillMaxSize()
             )
             Quadrant(
-                title = "Image composable",
-                description = "Creates a composable that lays out and draws a given Painter class object.",
+                title = stringResource(R.string.titre2),
+                description = stringResource(R.string.description2),
                 backgroundColor = Color(0xFFD0BCFF),
                 modifier = Modifier
                     .weight(1f)
@@ -104,16 +102,16 @@ fun QuadrantCompose(modifier: Modifier = Modifier) {
             modifier = Modifier.weight(1f)
         ) {
             Quadrant(
-                title = "Row composable",
-                description = "A layout composable that places its children in a horizontal sequence.",
+                title = stringResource(R.string.titre3),
+                description = stringResource(R.string.description3),
                 backgroundColor = Color(0xFFB69DF8),
                 modifier = Modifier
                     .weight(1f)
                     .fillMaxSize()
             )
             Quadrant(
-                title = "Column composable",
-                description = "A layout composable that places its children in a vertical sequence.",
+                title = stringResource(R.string.titre4),
+                description = stringResource(R.string.description4),
                 backgroundColor = Color(0xFFF6EDFF),
                 modifier = Modifier
                     .weight(1f)
